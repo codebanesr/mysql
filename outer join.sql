aefcde623eb78a3b1c3810ed9b801d1f66814a54
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-05-17 02:06:53 +0530

    Create outer join.sql

---
+++ outer join.sql
@@ -0,0 +1,11 @@
+
+SELECT a.company_id AS "a.ComID", 
+a.company_name AS "C_Name",
+b.company_id AS "b.ComID", 
+b.item_name AS "I_Name" 
+FROM   company a
+FULL OUTER JOIN foods b
+ON a.company_id = b.company_id
+WHERE a.company_id IS NULL 
+OR b.company_id IS NULL 
+ORDER BY company_name;
