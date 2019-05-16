a7b18f59cea6adcafde4f97794584313eab4284b
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-05-17 02:13:45 +0530

    Update outer join.sql

--- outer join.sql
+++ outer join.sql
@@ -13,3 +13,14 @@ ORDER BY company_name;
 
 
 https://www.w3resource.com/sql/joins/perform-a-full-outer-join.php
+
+Look at the diagrams
+
+SELECT a.company_id AS "a.ComID",
+a.company_name AS "C_Name",
+b.company_id AS "b.ComID", 
+b.item_name AS "I_Name" 
+FROM   company a 
+FULL OUTER JOIN foods b 
+ON a.company_id = b.company_id;
+
