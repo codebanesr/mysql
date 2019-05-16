4e47ec946cbfe81f0a1340ec3abde06449ae4879
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-05-17 02:07:18 +0530

    Update outer join.sql

--- outer join.sql
+++ outer join.sql
@@ -9,3 +9,7 @@ ON a.company_id = b.company_id
 WHERE a.company_id IS NULL 
 OR b.company_id IS NULL 
 ORDER BY company_name;
+
+
+
+https://www.w3resource.com/sql/joins/perform-a-full-outer-join.php
