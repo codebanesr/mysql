dedfaf5bfdc04077a1605a2ea63336c04354950c
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-06-05 05:38:38 +0530

    Update practical-problems.sql

--- practise/practical-problems.sql
+++ practise/practical-problems.sql
@@ -1,3 +1,6 @@
 #https://stackoverflow.com/questions/11312433/how-to-alter-a-column-and-change-the-default-value
 #change the entire structure of the column, changing data type as  well as the default value. use change or MODIFY
 ALTER TABLE foobar_data CHANGE COLUMN col col VARCHAR(255) NOT NULL DEFAULT '{}';
+or
+ALTER TABLE foobar_data MODIFY COLUMN col VARCHAR(255) NOT NULL DEFAULT '{}';
+
