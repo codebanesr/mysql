5dffc38ebb1c0a558773ba01beae91d48cf1927c
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-06-05 05:53:02 +0530

    Update practical-problems.sql

--- practise/practical-problems.sql
+++ practise/practical-problems.sql
@@ -4,3 +4,8 @@ ALTER TABLE foobar_data CHANGE COLUMN col col VARCHAR(255) NOT NULL DEFAULT '{}'
 or
 ALTER TABLE foobar_data MODIFY COLUMN col VARCHAR(255) NOT NULL DEFAULT '{}';
 
+-- set a default value for column inside the table, so now we are not changing the column but altering its property,
+-- use  alter command twice once for table the next time for column
+-- https://stackoverflow.com/questions/11312433/how-to-alter-a-column-and-change-the-default-value
+ALTER TABLE details ALTER name SET DEFAULT "shanur"
+
